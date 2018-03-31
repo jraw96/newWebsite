5c70835be0fc28b0610ef6237e7635406843f8ac
Jake Raw <jakeraw@Jakes-MacBook-Pro.local>
2018-03-31 00:23:35 -0400

    Added email to navbar

--- js/init.js
+++ js/init.js
@@ -50,14 +50,23 @@
 
            var active_section;
            
-           console.log("HEre is the active session: " + JSON.stringify(event) + " " + direction)
+
 
 			active_section = $(this);
 			if (direction === "up") active_section = active_section.prev();
 
-			var active_link = $('#nav-wrap a[href="#' + active_section.attr("id") + '"]');
+            
+            if(active_section.attr("id") == null){
+                console.log("oyoyoyoyoyo")
+                var active_link = $('#nav-wrap a[href="#resume"]');
+           
+            }else{
+            
+            var active_link = $('#nav-wrap a[href="#' + active_section.attr("id") + '"]');
+            }
+            console.log("Here is the active link: " + active_section.attr("id") )
 
-         navigation_links.parent().removeClass("current");
+            navigation_links.parent().removeClass("current");
 			active_link.parent().addClass("current");
 
 		},
